10e41308f01b41ed92e9ef869c3ebbc61ce77d90
Rodger Oliver <timeline.mb@gmail.com>
2019-09-01 21:20:42 -0300

    2 programação: refatoração de struct-simple.c

--- 2-semestre/disciplinas/c/struct-simple.c
+++ 2-semestre/disciplinas/c/struct-simple.c
@@ -2,50 +2,52 @@
 #include <stdlib.h>
 
 //Estrutura base do nó.
-struct node {
-	int nData;
-	struct node *pLink;
+struct Node {
+	int valor;
+	struct Node *next;
 };
 
-//Função pra printar o nó na tela.
-void displayLL(struct node *p) {
-	printf("Mostrando a lista:\n");
-	if(p) {
-		do {
-			printf("%d ", p->nData);
-			p=p->pLink;
-		}
-		while(p);
-	} else
-		printf("Lista vazia.");
-}
+void displayLL(struct Node *p);
 
 int main(void) {
-	struct node *inicio = NULL;
-	struct node *pNode1 = NULL;
-	struct node *pNode2 = NULL;
-	struct node *pNode3 = NULL;
+	struct Node *inicio = NULL;
+	struct Node *node1 = NULL;
+	struct Node *node2 = NULL;
+	struct Node *node3 = NULL;
 
 	//Criando os nos e associando os dados.
-	pNode1 = (struct node*) malloc(sizeof(struct node*));
-	inicio = pNode1;
-	pNode1->nData = 10;
+	node1 = (struct Node*) malloc(sizeof(struct Node*));
+	inicio = node1;
+	node1->valor = 10;
 
-	pNode2 = (struct node*) malloc(sizeof(struct node*));
-	pNode2->nData = 20;
+	node2 = (struct Node*) malloc(sizeof(struct Node*));
+	node2->valor = 20;
 
-	pNode3 = (struct node*) malloc(sizeof(struct node*));
-	pNode3->nData = 30;
+	node3 = (struct Node*) malloc(sizeof(struct Node*));
+	node3->valor = 30;
 
 	//Conectando os nós
-	pNode1->pLink = pNode2;
-	pNode2->pLink = pNode3;
-	pNode3->pLink = NULL;
+	node1->next = node2;
+	node2->next = node3;
+	node3->next = NULL;
 
 	//Mostrando a lista.
-	if(pNode1) {
+	if(inicio) {
 		displayLL(inicio);
 	}
 
 	return 0;
+}
+
+//Função pra printar o nó na tela.
+void displayLL(struct Node *p) {
+	printf("Mostrando a lista:\n");
+	if(p) {
+		do {
+			printf("%d ", p->valor);
+			p=p->next;
+		}
+		while(p);
+	} else
+		printf("Lista vazia.");
 }
