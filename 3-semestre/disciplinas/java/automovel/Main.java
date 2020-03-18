1d71da5dd73b31c2d615166d72188b3f79525cc1
Rodger Oliver <timeline.mb@gmail.com>
2020-03-18 13:03:33 -0300

    3 programação: automovel com formulário

--- 3-semestre/disciplinas/java/automovel/Main.java
+++ 3-semestre/disciplinas/java/automovel/Main.java
@@ -1,16 +1,36 @@
-import javax.swing.JOptionPane;
+import javax.swing.*;
 
 class Main {
 	public static void main(String[] args) {
 		String app_name = "Automovel v1.0";
 
-		String marca       = "Chevrolet";
-		String modelo      = "Onix";
-		String cor         = "Branco";
-		String combustivel = "Flex";
-		int ano            = 2020;
-		float preco        = 50000;
+		/* get data */
+		JTextField marca_field       = new JTextField();
+		JTextField modelo_field      = new JTextField();
+		JTextField cor_field         = new JTextField();
+		JTextField combustivel_field = new JTextField();
+		JTextField ano_field         = new JTextField();
+		JTextField preco_field       = new JTextField();
 
+		Object[] message = {
+			"Marca:", marca_field,
+			"Modelo:", modelo_field,
+			"Cor:", cor_field,
+			"Combustivel:", combustivel_field,
+			"Ano:", ano_field,
+			"Preco:", preco_field
+		};
+
+		int option = JOptionPane.showConfirmDialog(null, message, "Cadastro de Carro", JOptionPane.OK_CANCEL_OPTION);
+
+		String marca = marca_field.getText();
+		String modelo = modelo_field.getText();
+		String cor = cor_field.getText();
+		String combustivel = combustivel_field.getText();
+		int ano = Integer.parseInt(ano_field.getText());
+		float preco = Float.parseFloat(preco_field.getText());
+
+		/* create object */
 		Automovel carro1 = new Automovel(marca, modelo, cor, combustivel, ano, preco);
 
 		String output = carro1.toString();
